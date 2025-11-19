211e924789bc4358940ed7e96af29756ca4f2d0f
Juaneljuampi <ju.cortesn@duocuc.cl>
2025-11-19 02:27:57 -0300

    iniciado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <router>
-      <router>
-        <route path="/" element={<Home />} />
-      </router>
-    </router>
-    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
